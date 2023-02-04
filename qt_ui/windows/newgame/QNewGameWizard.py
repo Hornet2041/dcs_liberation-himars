bd45aa51caed20166b4e315dd534326d82f6cba2
Hornet2041 <55866858+Hornet2041@users.noreply.github.com>
2023-02-03 19:14:36 -0500

    Update QNewGameWizard.py

--- qt_ui/windows/newgame/QNewGameWizard.py
+++ qt_ui/windows/newgame/QNewGameWizard.py
@@ -169,6 +169,7 @@ class NewGameWizard(QtWidgets.QWizard):
             ov10a_bronco=self.field("ov10a_bronco"),
             frenchpack=self.field("frenchpack"),
             high_digit_sams=self.field("high_digit_sams"),
+            himars=self.field("himars"),
         )
 
         blue_faction = self.faction_selection_page.selected_blue_faction
@@ -673,6 +674,8 @@ class GeneratorOptions(QtWidgets.QWizardPage):
         self.registerField("frenchpack", frenchpack)
         high_digit_sams = QtWidgets.QCheckBox()
         self.registerField("high_digit_sams", high_digit_sams)
+        himars = QtWidgets.QCheckBox()
+        self.registerField("himars", himars)
 
         modHelpText = QtWidgets.QLabel(
             "<p>Select the mods you have installed. If your chosen factions support them, you'll be able to use these mods in your campaign.</p>"
@@ -724,6 +727,9 @@ class GeneratorOptions(QtWidgets.QWizardPage):
         modLayout_row += 1
         modLayout.addWidget(QtWidgets.QLabel("High Digit SAMs"), modLayout_row, 0)
         modLayout.addWidget(high_digit_sams, modLayout_row, 1)
+        modLayout_row += 1
+        modLayout.addWidget(QtWidgets.QLabel("Misc Military Assets: M142 HIMARS"), modLayout_row, 0)
+        modLayout.addWidget(himars, modLayout_row, 1)
         modSettingsGroup.setLayout(modLayout)
 
         mlayout = QVBoxLayout()
